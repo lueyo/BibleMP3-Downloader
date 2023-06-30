05b281fe6568f28b3157c426c75bd4fd32790920
lueyo <84575656+lueyo@users.noreply.github.com>
2023-06-30 18:39:34 +0200

    Update install.sh
    ahora funciona

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-!#/bin/bash
+#!/bin/bash
 echo Idiom es en it fr pt ab
-echo 6 es
-echo 1 en
-echo 7 fr
-echo 10 it
-echo 2 pt
-echo 16 ab
+echo "6 es"
+echo "1 en"
+echo "7 fr"
+echo "10 it"
+echo "2 pt"
+echo "16 ab"
 
 read i
 
-n = 1
-:e
-wget http://audio1.wordfree.net/bibles/app/audio/$i_$n.zip
-$n = $n+1
-goto e
+n=1
+while true
+do
+    wget http://audio1.wordfree.net/bibles/app/audio/"$i"_"$n".zip
+    ((n++))
+done
